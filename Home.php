751be42e1af10db16fb2c5a8ad6f5ed6673a8e64
YasmineBch123 <113337455+YasmineBch123@users.noreply.github.com>
2023-03-27 13:38:22 +0200

    change

---
+++ Home.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Home.css">
+    <title>Document</title>
+</head>
+<body>
+    
+<div id="mySidebar" class="sidebar">
+    <nav>
+        <ul>
+               
+                <li><a href="">Dashboard</a></li>
+                <li><a href="">Transaction</a></li>
+                <li><a href="">Wallet</a></li>
+                <label class="logo">Paypalooza</label>
+                <li><a href="">Log in</a></li>
+                <a href="" class="btn">Sign up</a>
+        </ul>
+     </nav>   
+<div id="image1">
+<img src="schermafbeelding 2023-03-27 133132.png">
+</div>
+</body>
+</html>
