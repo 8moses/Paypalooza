a9658306584572ec05935a6f2ae0a08ee30a6ecc
YasmineBch123 <113337455+YasmineBch123@users.noreply.github.com>
2023-03-29 11:31:56 +0200

    hkhio;

--- Home.php
+++ Home.php
@@ -4,12 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="Home.css">
+    <link rel="stylesheet" href="Home.css" rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
     
-<div id="mySidebar" class="sidebar">
+<div id="mySidebar" class="sidebar"></div>
     <nav>
         <ul>
                
@@ -21,8 +21,32 @@
                 <a href="" class="btn">Sign up</a>
         </ul>
      </nav>   
-<div id="image1">
-<img src="schermafbeelding 2023-03-27 133132.png">
-</div>
+     
+
+     <div id="image1"><img src="schermafbeelding 2023-03-27 133132.png"width="300px" height="400px"></div>
+     <div id="square">
+        <p>Securely pay and get paid with ease - powered by us.<p>
+     </div>
+     <div id="image2"><img src="schermafbeelding 2023-03-29 103733.png"width="50px" height="50px"></div>
+     <div id="image3"><img src="schermafbeelding 2023-03-29 105243.png"width="80px" height="80px"></div>
+     <h1>Pay with ease and 
+        confidence, <br>
+        with our secure online payment <br>
+        solution.</h1>
+
+    <div id="line"></div>
+    <h4>Welcome to our website! We are a secure and convenient<br>
+        online payment system that makes it easy for you to send<br>
+         and receive money. Whether you're paying for goods and<br>
+          services online, sending money to friends and family, or<br>
+           receiving payments for your business, we've got you covered. </h4>
+
+    <div id="signupbtn"><h2>Sign up</h2></div>
+    <div id="loginbtn"><h2>Log in</h2></div>
+ 
+
+
+
+
 </body>
 </html>
