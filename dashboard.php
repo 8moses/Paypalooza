08d361b86251bd4467cd4557536973a0b7c8deef
8moses <506869@vistacollege.nl>
2023-04-06 10:30:44 +0200

    dashboard
    dashboard

---
+++ dashboard.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="dashboard.css" rel="stylesheet">
+    
+</head>
+<body>
+<div id="mySidebar" class="sidebar"></div>
+<!-- Navigatiebalk -->
+    <nav>
+        <ul>
+                <li><a href="">Dashboard</a></li>
+                <li><a href="">Transaction</a></li>
+                <li><a href="">Wallet</a></li>
+                <label class="logo">Paypalooza</label>
+        </ul>
+     </nav>
+     <div id="image3"><img src="schermafbeelding 2023-03-29 105243.png"width="80px" height="80px"></div>
+
+     <!-- Geld overschrijven scherm -->
+     <div id="paypaloozasaldotext">
+        <h2>Paypalooza-saldo:</h2>
+     </div>
+     <div id="saldo">
+        <p>0,00  EUR</p>
+     </div>
+     <div id="beschikbaar">
+        <p>beschikbaar</p>
+     </div>
+     <div id="geldoverschrijvenbutton">
+        <p>Geld overschrijven</p>
+     </div>
+     <div id="welkomuser">
+      <p>Welcome back to Paypalooza,<br>user123</p>
+     </div>
+     <div id="squareachtergrond1"></div>
+
+     <div id="text1">
+        <p>Voeg geld toe met iDeal, zodat u direct<br> online kunt schoppen.</p>
+     </div>
+     <div id="geldtoevoegenideal"> 
+      <p>Geld toevoegen met ideal</p>
+     </div>
+     <div id="geldtoevoegentext">
+      <p>Geld toevoegen met iDeal</p>
+     </div>
+
+     <div id="plusknopimage"><img src="plusknop.png"width="20px" height="20px"></div>
+
+     <div id="bankrekeningcreditcardtext">
+      <p>Bankrekening en creditcards.</p>
+     </div>
+     <div id="ingbanktext">
+      <p>ING Bank</p>
+     </div>
+     <div id="creditcardenbankrekeningtext">
+      <p>Creditcard of bankrekening koppelen</p>
+     </div>
+     <div id="plusknopimage2"><img src="plusknop.png"width="20px" height="20px"></div>
+
+
+
+</body>
+</html>
